df23c307f7f13ea8a7ea1ca3da193e278390260a
Luan Liz <luanliz7@gmail.com>
2022-09-27 18:09:15 -0300

    first commit

---
+++ modules/express.js
@@ -0,0 +1,81 @@
+const { json } = require("express");
+const express = require("express");
+const UserModel = require("../src/models/user.model");
+const app = express();
+
+app.use(express.json());
+
+//middleware(são funçoes executadas antes de qualquer ação que o express realiza)
+app.use((req, res, next) => {
+  console.log(`Request Type: ${req.method}`);
+  console.log(`Content Type: ${req.headers["content-type"]}`);
+  console.log(`Date: ${new Date()}`);
+
+  next();
+});
+
+app.get("/home", (req, res) => {
+  res.status(200).send("<h1>hello Luan</h1>");
+});
+
+//pegar todos os usuários
+app.get("/users", async (req, res) => {
+  try {
+    const users = await UserModel.find({});
+    res.status(200).json(users);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+//Pegar usuário pelo id
+app.get("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    const user = await UserModel.findById(id);
+
+    return res.status(200).json(user);
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+});
+
+//Criar um usuário
+app.post("/users", async (req, res) => {
+  try {
+    const user = await UserModel.create(req.body);
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//Atualizar usuário
+
+app.patch("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndUpdate(id, req.body, { new: true });
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+//Deletar usuário
+
+app.delete("/users/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = await UserModel.findByIdAndRemove(id);
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+const port = 8080;
+app.listen(port, () => console.log(`Rodando na porta ${port}!`));
